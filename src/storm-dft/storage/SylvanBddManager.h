4041dbcadaca31324c0c2c6d3d72fcc08c3b375c
Matthias Volk <m.volk@utwente.nl>
2022-09-23 17:07:22 +0200

    Revised SylvanBddManager

--- src/storm-dft/storage/SylvanBddManager.h
+++ src/storm-dft/storage/SylvanBddManager.h
@@ -9,34 +9,27 @@
 namespace storm::dft {
 namespace storage {
 
-/**
- * Simple Manager for Sylvan
- *
- * \note
- * Not thread safe.
- *
- * Initializes Sylvan
- * which can only be initialized once per programm invocation
+/*!
+ * Simple Manager for Sylvan.
+ * Note that the manager is not thread safe.
+ * Initializes Sylvan which can only be initialized once per program invocation.
  */
 class SylvanBddManager {
    public:
-    /**
-     * Initializes Sylvan
-     *
-     * \note
-     * Internally Sylvan is initialized by a InternalSylvanDdManager.
-     * This ensures compatibility.
+    /*!
+     * Initialize Sylvan.
+     * Internally Sylvan is initialized by a InternalSylvanDdManager. This ensures compatibility.
      */
     SylvanBddManager() = default;
 
     // We can only initialize Sylvan once therefore no copy semantics
-    SylvanBddManager(SylvanBddManager const &) = delete;
+    SylvanBddManager(SylvanBddManager const&) = delete;
 
-    SylvanBddManager(SylvanBddManager &&) = default;
-    SylvanBddManager &operator=(SylvanBddManager &&) = default;
+    SylvanBddManager(SylvanBddManager&&) = default;
+    SylvanBddManager& operator=(SylvanBddManager&&) = default;
 
-    /**
-     * Destroys Sylvan
+    /*!
+     * Destructor.
      */
     ~SylvanBddManager() = default;
 
@@ -53,101 +46,95 @@ class SylvanBddManager {
     }
 
     /**
-     * Creates a variable with a unique name
+     * Create a variable with the given name.
      *
-     * \return Index of the variable
+     * @param name Variable name.
+     * @return Index of the variable.
      */
-    uint32_t createVariable(std::string const name) {
+    uint32_t createVariable(std::string const& name) {
         nameToIndex[name] = nextFreeVariableIndex;
         indexToName[nextFreeVariableIndex] = name;
         sylvan::Bdd::bddVar(nextFreeVariableIndex);
         return nextFreeVariableIndex++;
     }
 
-    /**
-     * \return
-     * The bdd representation of the variable.
-     * Therefore ITE(x, 1, 0).
+    /*!
+     * Get positive literal for variable.
+     * @param name Variable name.
+     * @return BDD representation of the variable: ITE(x, 1, 0).
      */
-    sylvan::Bdd getPositiveLiteral(std::string const name) const {
-        return sylvan::Bdd::bddVar(nameToIndex.at(name));
+    sylvan::Bdd getPositiveLiteral(std::string const& name) const {
+        return getPositiveLiteral(getIndex(name));
     }
 
-    /**
-     * \note equivalent with !getPositiveLiteral(name)
-     *
-     * \return
-     * The negative bdd representation of the variable.
-     * Therefore ITE(x, 0, 1).
+    /*!
+     * Get negative literal for variable.
+     * @param name Variable name.
+     * @return Negative BDD representation of the variable: ITE(x, 0, 1).
      */
-    sylvan::Bdd getNegativeLiteral(std::string const name) const {
-        return !sylvan::Bdd::bddVar(nameToIndex.at(name));
+    sylvan::Bdd getNegativeLiteral(std::string const& name) const {
+        return getNegativeLiteral(getIndex(name));
     }
 
-    /**
-     * \return
-     * The bdd representation of the variable.
-     * Therefore ITE(x, 1, 0).
+    /*!
+     * Get positive literal for variable.
+     * @param index Variable index.
+     * @return BDD representation of the variable: ITE(x, 1, 0).
      */
     sylvan::Bdd getPositiveLiteral(uint32_t const index) const {
         return sylvan::Bdd::bddVar(index);
     }
 
-    /**
-     * \note equivalent with !getPositiveLiteral(index)
-     *
-     * \return
-     * The negative bdd representation of the variable.
-     * Therefore ITE(x, 0, 1).
+    /*!
+     * Get negative literal for variable.
+     * @param index Variable index.
+     * @return Negative BDD representation of the variable: ITE(x, 0, 1).
      */
     sylvan::Bdd getNegativeLiteral(uint32_t const index) const {
         return !sylvan::Bdd::bddVar(index);
     }
 
-    /**
-     * \return
-     * The bdd representation of the constant function 1.
+    /*!
+     * Get BDD representation of the constant function 1.
+     * @return BDD representing 1.
      */
-    sylvan::Bdd getOne() {
+    sylvan::Bdd getOne() const {
         return sylvan::Bdd::bddOne();
     }
 
-    /**
-     * \return
-     * The bdd representation of the constant function 0.
+    /*!
+     * Get BDD representation of the constant function 0.
+     * @return BDD representing 0.
      */
-    sylvan::Bdd getZero() {
+    sylvan::Bdd getZero() const {
         return sylvan::Bdd::bddZero();
     }
 
-    /**
-     * \return
-     * The index of the variable with the given name.
+    /*!
+     * Get index of variable with given name.
+     * @param name Variable name.
+     * @return Index.
      */
-    uint32_t getIndex(std::string const name) const {
+    uint32_t getIndex(std::string const& name) const {
         return nameToIndex.at(name);
     }
 
-    /**
-     * \return
-     * The name of the variable with the given index.
+    /*!
+     * Get name of variable at given index.
+     * @param index Variable index.
+     * @return Name.
      */
     std::string getName(uint32_t const index) const {
         return indexToName.at(index);
     }
 
-    /**
-     * Exports the given Bdd to a file
-     * in the dot format.
-     *
-     * \param bdd
-     * The Bdd.
-     * \param filename
-     * The name of the file the dot graph is written to
+    /*!
+     * Export given BDD to a file in the dot format.
+     * @param bdd BDD.
+     * @param filename Name of file.
      */
-    static void exportBddToDot(sylvan::Bdd const &bdd, std::string const &filename) {
-        FILE *filePointer = fopen(filename.c_str(), "w+");
-
+    static void exportBddToDot(sylvan::Bdd const& bdd, std::string const& filename) {
+        FILE* filePointer = fopen(filename.c_str(), "w+");
         // fopen returns a nullptr on failure
         if (filePointer == nullptr) {
             STORM_LOG_ERROR("Failure to open file: " << filename);
