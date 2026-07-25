793160334f44975ba0ce66cbcbf206bc3e201649
Matthias Volk <volkm@users.noreply.github.com>
2026-07-25 15:06:03 +0200

    Fixed missing namespace (#954)
    Should fix the issue in [the stormpy CI
    run](https://github.com/stormchecker/stormpy/actions/runs/30117531887/job/89561775109).

--- src/storm-dft/storage/DFTStateGenerationInfo.h
+++ src/storm-dft/storage/DFTStateGenerationInfo.h
@@ -26,7 +26,7 @@ class DFTStateGenerationInfo {
         : mUsageInfoBits(getUsageInfoBits(maxSpareChildCount)),
           stateIndexSize(getStateVectorSize(nrElements, nrOfSpares, nrRepresentatives, maxSpareChildCount)),
           mIdToStateIndex(nrElements) {
-        STORM_LOG_ASSERT(maxSpareChildCount < pow(2, mUsageInfoBits), "Bit length incorrect.");
+        STORM_LOG_ASSERT(maxSpareChildCount < std::pow(2, mUsageInfoBits), "Bit length incorrect.");
     }
 
     /*!
