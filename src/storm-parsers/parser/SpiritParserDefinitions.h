bcf05b33ee37b21f6702905a69ebe7a65c9f41eb
Matthias Volk <m.volk@tue.nl>
2026-09-09 09:45:52 +0200

    Fixes for includes and pragma push

--- src/storm-parsers/parser/SpiritParserDefinitions.h
+++ src/storm-parsers/parser/SpiritParserDefinitions.h
@@ -5,7 +5,6 @@
 
 // Boost Spirit's utf8.hpp uses char_traits<ucs4_char> which Apple libc++ (Xcode 26+) deprecated
 #if defined(__clang__) && defined(__apple_build_version__)
-#pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #endif
 
